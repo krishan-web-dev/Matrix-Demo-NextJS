ff24e1d463335ea6eaeeca41bb1d2508f1e4df7d
krishan-web-dev <55198018+krishan-web-dev@users.noreply.github.com>
2025-01-15 14:43:52 +0530

    Update 5

---
+++ src/components/blocks/slider/Shader-Slider.tsx
@@ -0,0 +1,12 @@
+//  import SwiperGL module
+import SwiperGL from '@/plugins/shaders-slider/dist/swiper-gl.esm.js';
+// import SwiperGL styles
+import '@/plugins/shaders-slider/dist/swiper-gl.scss';
+
+export default function ShaderSlider() {
+    return (
+        <section>
+
+        </section>
+    );
+}
