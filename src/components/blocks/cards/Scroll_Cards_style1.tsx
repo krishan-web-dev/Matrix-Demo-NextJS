f2a1ab7c1ef1712662667a3d9ba8eed140ad0a6e
krishan-web-dev <55198018+krishan-web-dev@users.noreply.github.com>
2025-01-25 13:19:13 +0530

    Cards New

--- src/components/blocks/cards/Scroll_Cards_style1.tsx
+++ src/components/blocks/cards/Scroll_Cards_style1.tsx
@@ -3,9 +3,13 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image'
+import NextLink from "@/components/reuseable/links/NextLink";
+
 
 import 'swiper/css';
-import './scroll_cards.scss';
+//import './scroll_cards.scss';
 import './scroll_style1.scss';
 
 export default function Scroll_Cards1() {
@@ -32,19 +36,19 @@ export default function Scroll_Cards1() {
             link: '#',
         },
         {
-            img: 'https://www.dimolanka.com/wp-content/uploads/2023/03/Agriculture-800x800-.jpg',
+            img: '/img/car-parking.jpg',
             title: 'Material Handling Equipement',
             subtitle: 'Advanced farming solutions for a sustainable future',
             link: '#',
         },
         {
-            img: '/img/fork-lift.jpg',
+            img: '/img/auto-mobile-equipment.jpg',
             title: 'Automobile Maintenance Equipment',
             subtitle: 'Specializing in material handling solutions with quality, safety, and value.',
             link: '#',
         },
         {
-            img: 'https://www.dimolanka.com/wp-content/uploads/2023/03/Agriculture-800x800-.jpg',
+            img: '/img/car-parking.jpg',
             title: 'Car Parking Solutions',
             subtitle: 'Advanced farming solutions for a sustainable future',
             link: '#',
@@ -56,7 +60,7 @@ export default function Scroll_Cards1() {
             link: '#',
         },
         {
-            img: 'https://www.dimolanka.com/wp-content/uploads/2023/03/Agriculture-800x800-.jpg',
+            img: '/img/auto-mobile-equipment.jpg',
             title: 'Welding Products',
             subtitle: 'Advanced farming solutions for a sustainable future',
             link: '#',
@@ -68,7 +72,7 @@ export default function Scroll_Cards1() {
             link: '#',
         },
         {
-            img: 'https://www.dimolanka.com/wp-content/uploads/2023/03/Agriculture-800x800-.jpg',
+            img: '/img/auto-mobile-equipment.jpg',
             title: 'Solar Systems',
             subtitle: 'Advanced farming solutions for a sustainable future',
             link: '#',
@@ -141,40 +145,45 @@ export default function Scroll_Cards1() {
                                 </Swiper>
                             </div>
                         ) : (
-                            <div className="discover-list hm-discover-carousel grid style-1">
+                            <div className="discover-list hm-discover-carousel grid style-1 cards__scroll">
                                 {cards.map((card, index) => (
-                                    <div className="single-item box" key={index}>
-                                        <a href={card.link} target="_blank" rel="noopener noreferrer">
-                                            <div className="abs-img">
-                                                <img src={card.img} alt={card.title} />
-                                            </div>
-                                            <div className="title-wrapper">
-                                                <span className="title">{card.title}</span>
-                                                <span className="sub-title">{card.subtitle}</span>
-                                            </div>
-                                            <div className="BlogTile_Button__SAl_F">
-                                                <div className="Button_Button__vVAZK" title="Click here">
-                                                    <span>
-                                                        <div className="Button_ArrowCont__WrhN_">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-                                                                <path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#F9F8F6"></path></svg>
-                                                            <div className="Button_Arrow__LawjV">
-                                                                <svg width="31" height="28" viewBox="0 0 31 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                                    <path d="M0.857198 13.7372L27.9141 13.7372" stroke="black" strokeWidth="3"></path>
-                                                                    <path d="M15.4561 1.39417L27.9142 13.8522L15.4561 26.3104" stroke="black" strokeWidth="3"></path>
-                                                                </svg>
-                                                            </div>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-                                                                <path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#F9F8F6">
-                                                                </path>
+                                    <figure className="overlay caption caption-overlay rounded-md single__card" key={index}>
+                                        <Link href="#">
+                                            <Image src={card.img} alt={card.title} width={600} height={600} />
+                                            <span className="bg" />
+                                        </Link>
+
+                                        <figcaption>
+                                            <h2 className="post-title h3 mt-1 mb-3">
+                                                <NextLink title={card.title} href="#" />
+                                            </h2>
+                                            <span className="sub-title">{card.subtitle}</span>
+                                        </figcaption>
+                                        <div className="BlogTile_Button__SAl_F">
+                                            <div className="Button_Button__vVAZK" title="Click here">
+                                                <span>
+                                                    <div className="Button_ArrowCont__WrhN_">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                                                            <path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#F9F8F6"></path></svg>
+                                                        <div className="Button_Arrow__LawjV">
+                                                            <svg width="31" height="28" viewBox="0 0 31 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                <path d="M0.857198 13.7372L27.9141 13.7372" stroke="black" strokeWidth="3"></path>
+                                                                <path d="M15.4561 1.39417L27.9142 13.8522L15.4561 26.3104" stroke="black" strokeWidth="3"></path>
                                                             </svg>
                                                         </div>
-                                                    </span>
-                                                </div>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                                                            <path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#F9F8F6">
+                                                            </path>
+                                                        </svg>
+                                                    </div>
+                                                </span>
                                             </div>
-                                        </a>
-                                    </div>
+                                        </div>
+                                    </figure>
                                 ))}
+
+
+
                             </div>
                         )}
                         {/* Scroll Card End */}
